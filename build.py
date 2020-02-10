f9ed0130d704e4d10f9e7277f29ca896b6b62aa4
Uilian Ries <uilianries@gmail.com>
2020-02-10 13:36:27 -0300

    Revert "Use target arch as build arch"
    This reverts commit 449f8a6b5895d7bded31dba18188b9fb3c88a6d3.

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import copy
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    base = {"os": "Windows", "compiler": "gcc", "compiler.version": "7",
+    base = {"os_build": "Windows", "compiler": "gcc", "compiler.version": "7",
             "compiler.exception": "seh", "compiler.libcxx": "libstdc++",
             "compiler.threads": "posix"}
     for version in ["4.9", "5", "6", "7", "8"]:
@@ -15,11 +15,13 @@ if __name__ == "__main__":
             tmp2["compiler.threads"] = th
             for ex in ["seh", "sjlj"]:
                 tmp3 = copy.copy(tmp2)
+                tmp3["arch_build"] = "x86_64"
                 tmp3["arch"] = "x86_64"
                 tmp3["compiler.exception"] = ex
                 builder.add(tmp3, {}, {}, {})
             for ex in ["dwarf2", "sjlj"]:
                 tmp3 = copy.copy(tmp2)
+                tmp3["arch_build"] = "x86"
                 tmp3["arch"] = "x86"
                 tmp3["compiler.exception"] = ex
                 builder.add(tmp3, {}, {}, {})
--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,6 @@ class MingwInstallerConan(ConanFile):
 
     settings = {"os_build": ["Windows"],
                 "arch_build" : ["x86", "x86_64"],
-                "arch": ["x86", "x86_64"],
                 "compiler": {"gcc": {"version": None,
                                      "threads": ["posix", "win32"],
                                      "exception": ["dwarf2", "sjlj", "seh"]}}}
@@ -29,7 +28,7 @@ class MingwInstallerConan(ConanFile):
     def build(self):
         self.output.info("Updating MinGW List ... please wait.")
 
-        installer = get_best_installer(str(self.settings.arch),
+        installer = get_best_installer(str(self.settings.arch_build),
                                        str(self.settings.compiler.threads),
                                        str(self.settings.compiler.exception),
                                        str(self.settings.compiler.version))
@@ -47,8 +46,6 @@ class MingwInstallerConan(ConanFile):
 
     def package_id(self):
         self.info.include_build_settings()
-        self.info.settings.arch_build = self.info.settings.arch
-        del self.info.settings.arch
 
     def package_info(self):
         self.env_info.path.append(os.path.join(self.package_folder, "bin"))
